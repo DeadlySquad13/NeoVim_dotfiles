2b56aca563d1d028fcc7425630a04bc654991e87
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2025-02-22 14:16:30 +0300

    [Chore](Hydra): Changed repo.

--- lua/ds_omega/config/Editing/multicursors/init.lua
+++ lua/ds_omega/config/Editing/multicursors/init.lua
@@ -2,9 +2,8 @@ return {
     "smoka7/multicursors.nvim",
     event = "VeryLazy",
     dependencies = {
-        'nvimtools/hydra.nvim',
+        "nvimtools/hydra.nvim",
     },
-    -- version = "v1.0.0",
     opts = {},
     cmd = { 'MCstart', 'MCvisual', 'MCclear', 'MCpattern', 'MCvisualPattern', 'MCunderCursor' },
     keys = {
